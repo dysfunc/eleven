a4d142d3adf3875fe84610c74b9125eff6a43473
Kieran Boyle <kboyle@yahoo-inc.com>
2017-07-24 13:53:26 -0700

    add gradient to weather plugin for text visibility

--- app/js/demo.js
+++ app/js/demo.js
@@ -208,6 +208,7 @@
       // Eleven().getPlugin('news').headlines()
       // Eleven().getPlugin('weather').forecast('San Francisco');
       // Eleven().getPlugin('weather').forecast('New York');
+      // Eleven().getPlugin('weather').forecast('Los Angeles');
     }
 
     $('#run').on('click', function(){
